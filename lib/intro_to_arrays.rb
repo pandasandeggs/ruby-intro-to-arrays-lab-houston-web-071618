4e5c86f5e2016cfff90b07b0730b884354e294db
Chantal Wallace <theteatreehouse@gmail.com>
2018-06-22 01:29:02 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,7 +4,19 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
-  my_two_array = array_with_two_elements
+  my_two_array = [1,2]
   return my_two_array
+end
+
+def first_element
+  return array[0]
+end
+
+def third_element
+  return array[2]
+end
+
+def last_element
+  return array[-1]
 end
 
